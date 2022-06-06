4e05f3d1276c943fd772a2f703d8f5bc5ba1a49c
Waleed Khan <me@waleedkhan.name>
2022-06-06 16:31:27 -0700

    feat(scl): make `Node` an opaque associated type

--- segmented-changelog/src/lib.rs
+++ segmented-changelog/src/lib.rs
@@ -1,15 +1,13 @@
 use rusqlite::named_params;
 use std::collections::HashSet;
+use std::fmt::Debug;
+use std::hash::Hash;
 use std::path::Path;
 
 use thiserror::Error;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct NodeId(usize);
-
-pub type NodeName = [u8];
-
-pub type NodeNameOwned = <NodeName as ToOwned>::Owned;
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -19,67 +17,82 @@ pub enum Error {
     #[error("Could not initialize database: {0}")]
     InitializeDatabase(rusqlite::Error),
 
-    #[error("Could not create node {name:?}: {error}")]
+    #[error("Could not create node {node:?}: {error}")]
     CreateNode {
-        name: Vec<u8>,
+        node: Box<dyn Debug>,
         error: rusqlite::Error,
     },
 
-    #[error("Could not query for ID for node {name:?}: {error}")]
+    #[error("Could not query for ID for node {node:?}: {error}")]
     QueryNodeId {
-        name: Vec<u8>,
+        node: Box<dyn Debug>,
         error: rusqlite::Error,
     },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-pub struct Dag {
+pub trait DagBackend {
+    type Node: Clone + Debug + Eq + Ord + Hash + AsRef<[u8]> + 'static;
+
+    fn get_parents(&self, node: &Self::Node) -> Vec<Self::Node>;
+}
+
+pub struct Dag<B: DagBackend> {
+    backend: B,
     db: rusqlite::Connection,
 }
 
-impl Dag {
-    pub fn open(path: &Path) -> Result<Self> {
+impl<B: DagBackend> Dag<B> {
+    pub fn open(backend: B, path: &Path) -> Result<Self> {
         let conn = rusqlite::Connection::open(path).map_err(Error::OpenDatabase)?;
-        let dag = Self::open_from_conn(conn)?;
+        let dag = Self::open_from_conn(backend, conn)?;
         Ok(dag)
     }
 
-    pub fn open_from_conn(conn: rusqlite::Connection) -> Result<Self> {
-        let mut dag = Dag { db: conn };
+    pub fn open_from_conn(backend: B, conn: rusqlite::Connection) -> Result<Self> {
+        let mut dag = Dag { backend, db: conn };
         dag.init_db()?;
         Ok(dag)
     }
 
     fn init_db(&mut self) -> Result<()> {
         self.db
-            .execute(
+            .execute_batch(
                 r#"
 CREATE TABLE IF NOT EXISTS
-names (name BLOB)
+names (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name BLOB NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS
+segments (
+    parents TEXT NOT NULL,
+    start INTEGER NOT NULL,
+    end INTEGER NOT NULL
+);
 "#,
-                [],
             )
             .map_err(Error::InitializeDatabase)?;
         Ok(())
     }
 
-    pub fn make_node(
-        &mut self,
-        node: &NodeName,
-        get_parents: impl Fn(&NodeName) -> Vec<NodeNameOwned>,
-    ) -> Result<NodeId> {
+    pub fn make_node(&mut self, node: &B::Node) -> Result<NodeId> {
         let all_nodes = {
             let mut acc = HashSet::new();
             let mut next = {
                 let mut next = HashSet::new();
-                next.insert(node.to_owned());
+                next.insert(node.clone());
                 next
             };
 
             // TODO: Be sure to traverse depth-first, in order to assign contiguous sequences of IDs.
             while !next.is_empty() {
-                let parents: HashSet<_> = next.iter().flat_map(|node| get_parents(node)).collect();
+                let parents: HashSet<_> = next
+                    .iter()
+                    .flat_map(|node| self.backend.get_parents(node))
+                    .collect();
                 acc.extend(next.into_iter());
 
                 // TODO: Filter any parents which have already been registered with the database.
@@ -94,15 +107,15 @@ names (name BLOB)
             self.db
                 .execute(
                     "
-INSERT INTO names
+INSERT INTO names (name)
 VALUES (:name)
 ",
                     named_params! {
-                        ":name": node,
+                        ":name": node.as_ref(),
                     },
                 )
                 .map_err(|error| Error::CreateNode {
-                    name: node.to_vec(),
+                    node: Box::new(node.clone()),
                     error,
                 })?;
         }
@@ -115,11 +128,11 @@ SELECT rowid
 FROM names
 WHERE name = :name
 ",
-                named_params! {":name": node},
+                named_params! {":name": node.as_ref()},
                 |row| row.get(0),
             )
             .map_err(|error| Error::QueryNodeId {
-                name: node.to_vec(),
+                node: Box::new(node.clone()),
                 error,
             })?;
         let id = NodeId(id);
@@ -131,22 +144,34 @@ WHERE name = :name
 mod tests {
     use super::*;
 
-    fn make_in_memory_dag() -> Result<Dag> {
+    struct TestingDagBackend;
+
+    impl DagBackend for TestingDagBackend {
+        type Node = Vec<u8>;
+
+        fn get_parents(&self, node: &Self::Node) -> Vec<Self::Node> {
+            match node.as_slice() {
+                b"foo" => vec![b"bar".to_vec()],
+                _ => vec![],
+            }
+        }
+    }
+
+    type TestingDag = Dag<TestingDagBackend>;
+
+    fn make_in_memory_dag() -> Result<TestingDag> {
         let conn = rusqlite::Connection::open_in_memory().map_err(Error::InitializeDatabase)?;
-        let dag = Dag::open_from_conn(conn)?;
+        let dag = Dag::open_from_conn(TestingDagBackend, conn)?;
         Ok(dag)
     }
 
     #[test]
     fn test_init() -> Result<()> {
         let mut dag = make_in_memory_dag()?;
-        let foo_id = dag.make_node(b"foo", |name| match name {
-            b"foo" => vec![b"bar".to_vec()],
-            _ => vec![],
-        })?;
+        let foo_id = dag.make_node(&b"foo".to_vec())?;
         assert_eq!(foo_id, NodeId(2));
 
-        let bar_id = dag.make_node(b"bar", |_| Default::default())?;
+        let bar_id = dag.make_node(&b"bar".to_vec())?;
         assert_eq!(bar_id, NodeId(1));
 
         Ok(())
