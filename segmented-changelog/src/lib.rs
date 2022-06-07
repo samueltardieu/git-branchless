dce36387e34320ddcf39034ff00593dd76320e52
Waleed Khan <me@waleedkhan.name>
2022-06-06 17:36:38 -0700

    feat(scl): assign node IDs depth-first

--- segmented-changelog/src/lib.rs
+++ segmented-changelog/src/lib.rs
@@ -1,12 +1,12 @@
-use rusqlite::named_params;
-use std::collections::HashSet;
+use rusqlite::{named_params, OptionalExtension};
+use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::path::Path;
 
 use thiserror::Error;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct NodeId(usize);
 
 #[derive(Debug, Error)]
@@ -17,14 +17,23 @@ pub enum Error {
     #[error("Could not initialize database: {0}")]
     InitializeDatabase(rusqlite::Error),
 
-    #[error("Could not create node {node:?}: {error}")]
-    CreateNode {
+    #[error("Could not query for ID for node {node:?}: {error}")]
+    QueryNodeId {
         node: Box<dyn Debug>,
         error: rusqlite::Error,
     },
 
-    #[error("Could not query for ID for node {node:?}: {error}")]
-    QueryNodeId {
+    #[error("Could not build in-memory graph: {0}")]
+    BuildGraph(Box<Error>),
+
+    #[error("Could not assign ID for node {node:?}: {error}")]
+    AssignNodeIds {
+        node: Box<dyn Debug>,
+        error: Box<Error>,
+    },
+
+    #[error("Could not create node {node:?}: {error}")]
+    AssignNodeId {
         node: Box<dyn Debug>,
         error: rusqlite::Error,
     },
@@ -62,8 +71,8 @@ impl<B: DagBackend> Dag<B> {
                 r#"
 CREATE TABLE IF NOT EXISTS
 names (
-    id INTEGER PRIMARY KEY NOT NULL,
-    name BLOB NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name BLOB NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS
@@ -79,52 +88,20 @@ segments (
     }
 
     pub fn make_node(&mut self, node: &B::Node) -> Result<NodeId> {
-        let all_nodes = {
-            let mut acc = HashSet::new();
-            let mut next = {
-                let mut next = HashSet::new();
-                next.insert(node.clone());
-                next
-            };
-
-            // TODO: Be sure to traverse depth-first, in order to assign contiguous sequences of IDs.
-            while !next.is_empty() {
-                let parents: HashSet<_> = next
-                    .iter()
-                    .flat_map(|node| self.backend.get_parents(node))
-                    .collect();
-                acc.extend(next.into_iter());
-
-                // TODO: Filter any parents which have already been registered with the database.
-                next = parents;
-            }
-            let mut acc: Vec<_> = acc.into_iter().collect();
-            acc.sort();
-            acc
-        };
-
-        for node in all_nodes {
-            self.db
-                .execute(
-                    "
-INSERT INTO names (name)
-VALUES (:name)
-",
-                    named_params! {
-                        ":name": node.as_ref(),
-                    },
-                )
-                .map_err(|error| Error::CreateNode {
-                    node: Box::new(node.clone()),
-                    error,
-                })?;
-        }
+        let graph = self
+            .build_graph(node.clone())
+            .map_err(|error| Error::BuildGraph(Box::new(error)))?;
+        self.assign_node_ids(&graph, node)
+            .map_err(|error| Error::AssignNodeIds {
+                node: Box::new(node.clone()),
+                error: Box::new(error),
+            })?;
 
         let id: usize = self
             .db
             .query_row(
                 "
-SELECT rowid
+SELECT id
 FROM names
 WHERE name = :name
 ",
@@ -138,6 +115,138 @@ WHERE name = :name
         let id = NodeId(id);
         Ok(id)
     }
+
+    /// Build an in-memory representation of the DAG, determined by repeatedly
+    /// traversing the parents of `node`.
+    ///
+    /// The graph is a mapping from node to parent nodes.
+    ///
+    /// This graph may not be complete. If a node has already been registered
+    /// in the database, then it may be omitted as a key from the resulting graph.
+    fn build_graph(&self, node: B::Node) -> Result<HashMap<B::Node, Vec<B::Node>>> {
+        let mut current = {
+            let mut current = HashSet::new();
+            current.insert(node);
+            current
+        };
+        let mut graph = HashMap::new();
+        while !current.is_empty() {
+            let mut next = HashSet::new();
+            for node in current {
+                if graph.contains_key(&node) {
+                    continue;
+                }
+                if self.query_node_id(&node)?.is_some() {
+                    // FIXME: Issuing a database query for each node may be
+                    // expensive. It might be better to issue a query only
+                    // once every few nodes instead.
+                    continue;
+                }
+
+                let parents = self.backend.get_parents(&node);
+                graph.insert(node, parents.clone());
+                next.extend(parents);
+            }
+            current = next;
+        }
+        Ok(graph)
+    }
+
+    fn assign_node_ids(
+        &self,
+        graph: &HashMap<B::Node, Vec<B::Node>>,
+        initial_node: &B::Node,
+    ) -> Result<()> {
+        #[derive(Debug)]
+        enum State<T> {
+            WaitingForParents(T),
+            ReadyToAssign(T),
+        }
+
+        let mut nodes_to_assign: Vec<State<&B::Node>> = vec![State::WaitingForParents(initial_node)];
+        let mut assigned_nodes: HashSet<&B::Node> = HashSet::new();
+        while let Some(node_to_assign) = nodes_to_assign.pop() {
+            match node_to_assign {
+                State::WaitingForParents(node_to_assign) => {
+                    let parents = match graph.get(node_to_assign) {
+                        Some(parents) => parents,
+                        None => {
+                            // This was a leaf node, which indicates that it was already assigned an ID. We can skip this node.
+                            continue;
+                        }
+                    };
+
+                    // Be sure to insert the parent nodes such that the first parent is at the top. We want to traverse the first parents first to provide a contiguous sequence of IDs.
+                    nodes_to_assign.push(State::ReadyToAssign(node_to_assign));
+                    nodes_to_assign.extend(parents.iter().rev().map(State::WaitingForParents));
+                }
+
+                State::ReadyToAssign(node_to_assign) => {
+                    // Skip reassigning the same node if we've already seen it (such as
+                    // if two nodes have the same parent).
+                    if !assigned_nodes.insert(node_to_assign) {
+                        continue;
+                    }
+
+                    let result = self.db.execute(
+                        "
+INSERT INTO names (name)
+VALUES (:name)
+",
+                        named_params! {
+                            ":name": node_to_assign.as_ref(),
+                        },
+                    );
+                    match result {
+                        Ok(_) => {
+                            // ID assignment succeeded, do nothing.
+                        }
+
+                        Err(rusqlite::Error::SqliteFailure(
+                            rusqlite::ffi::Error {
+                                code: rusqlite::ErrorCode::ConstraintViolation,
+                                ..
+                            },
+                            _,
+                        )) => {
+                            // An ID was already assigned for this element, do nothing.
+                        }
+
+                        Err(error) => {
+                            return Err(Error::AssignNodeId {
+                                node: Box::new(initial_node.clone()),
+                                error,
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn query_node_id(&self, node: &B::Node) -> Result<Option<NodeId>> {
+        let node_id: Option<usize> = self
+            .db
+            .query_row(
+                "
+SELECT id
+FROM names
+WHERE name = :name
+        ",
+                named_params! {
+                    ":name": node.as_ref(),
+                },
+                |row| row.get(0),
+            )
+            .optional()
+            .map_err(|error| Error::QueryNodeId {
+                node: Box::new(node.clone()),
+                error,
+            })?;
+        Ok(node_id.map(NodeId))
+    }
 }
 
 #[cfg(test)]
@@ -147,11 +256,11 @@ mod tests {
     struct TestingDagBackend;
 
     impl DagBackend for TestingDagBackend {
-        type Node = Vec<u8>;
+        type Node = String;
 
         fn get_parents(&self, node: &Self::Node) -> Vec<Self::Node> {
-            match node.as_slice() {
-                b"foo" => vec![b"bar".to_vec()],
+            match node.as_str() {
+                "foo" => vec!["bar".to_string()],
                 _ => vec![],
             }
         }
@@ -166,12 +275,15 @@ mod tests {
     }
 
     #[test]
-    fn test_init() -> Result<()> {
+    fn test_id_assignment() -> Result<()> {
         let mut dag = make_in_memory_dag()?;
-        let foo_id = dag.make_node(&b"foo".to_vec())?;
-        assert_eq!(foo_id, NodeId(2));
+        let foo_id = dag.make_node(&"foo".to_string())?;
+        let foo_id2 = dag.make_node(&"foo".to_string())?;
+        assert_eq!(foo_id, foo_id2);
 
-        let bar_id = dag.make_node(&b"bar".to_vec())?;
+        let bar_id = dag.make_node(&"bar".to_string())?;
+        assert!(bar_id < foo_id, "parent node {bar_id:?} should have smaller ID (been assigned first) than child node {foo_id:?}");
+        assert_eq!(foo_id, NodeId(2));
         assert_eq!(bar_id, NodeId(1));
 
         Ok(())
